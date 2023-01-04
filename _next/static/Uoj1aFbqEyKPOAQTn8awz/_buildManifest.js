f7d0b9ad29f146917bb30028fac42330126de1a6
jym272 <jym272@users.noreply.github.com>
2023-01-04 00:26:03 +0000

    Deploying to gh-pages from @ jym272/github-actions-first@f2e847218968c2a7767849ad4b43050237546ea6 🚀

--- _next/static/Uoj1aFbqEyKPOAQTn8awz/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/07a819110b4b63e2.css","static/chunks/pages/index-be8e05efbd284374.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
