9b485e32d48cf97a8e0df81cae5cbc090a751e80
jorge <jym272@pop-os.localdomain>
2023-01-04 14:34:12 -0300

    some commento [skip actions]

--- frontend/UI/Buttons/ResetButton/index.tsx
+++ frontend/UI/Buttons/ResetButton/index.tsx
@@ -1,5 +1,5 @@
 import styles from './ResetButton.module.css'
-
+// SOME DUMMY COMMENTO
 export const ResetButton =({reset}:ResetButtonProps) => {
     return <div onClick={reset} className={styles["modern-button"]}>Reset</div>
 }
