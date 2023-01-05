b0b3da7a2f1dbfd671fbfb7c6c9fa8666631e56d
Jorge Clavijo <jym272@gmail.com>
2023-01-05 03:51:51 -0300

    deploy outputs refactor with GITHUB_OUTPUT env var

--- cmd.sh
+++ cmd.sh
@@ -1,12 +1,19 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
+
+#define a string var
 
 
+# save this cmd in a string du -h . | sort -hr
+echo "FILES=$(du  -h frontend/out | sort -hr)" >> NEW_VAR
+
+
+
+exit 0
 # folder of the work dir
 work_dir=$(pwd)
 only_the_name=$(basename $work_dir)
 echo "work dir: $work_dir"
 
 
-echo "from:\n$(ls frontend)"
 
 printf 'Name:\n%s\n' "$(ls frontend)"
