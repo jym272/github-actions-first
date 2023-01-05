3569c05245e16bc51ff2d7d0b0f571a92c4d375c
Jorge Clavijo <jym272@gmail.com>
2023-01-05 17:47:04 -0300

    updating output action 7

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import { Counter } from '../components';
 
+// Silly comment
 export default function Home() {
   return (
     <>
